8548447d0f8e76557051d54b59381e51365f82c8
Shija-cmd <shijajuma@protonmail.com>
2024-10-13 23:08:46 +0300

    Editing front end pages

--- elearning/urls.py
+++ elearning/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='index'),
+    path('404', views.error, name='404'),
+    path('about', views.about, name='about'),
+    path('contact', views.contact, name='contact'),
+    path('courses', views.courses, name='courses'),
+    path('team', views.team, name='team'),
+    path('testimonial', views.testimonial, name='testimonial')
 ]
--- elearning/views.py
+++ elearning/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
+
+def error(request):
+    return render(request, '404.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def courses(request):
+    return render(request, 'courses.html')
+
+def team(request):
+    return render(request, 'team.html')
+
+def testimonial(request):
+    return render(request, 'testimonial.html')
